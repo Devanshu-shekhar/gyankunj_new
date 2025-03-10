98af10375402917c47b2f06b548261b9b2832abe
Raj Pathar <raj.pathar93@gmail.com>
2025-03-10 23:04:16 +0530

    changes for fees payment

--- src/Components/Principal/Finance/Report/AdmissionFeesView.js
+++ src/Components/Principal/Finance/Report/AdmissionFeesView.js
@@ -1,24 +1,29 @@
 import React, { useState, useMemo, useEffect, useCallback } from "react";
-import { Typography } from "@mui/material";
-import { fetchAdmissionFeesInfo } from "../../../../ApiClient";
+import { Button, Typography } from "@mui/material";
+import { fetchAdmissionFeesInfo, fetchPaymentModes } from "../../../../ApiClient";
 import CommonMatTable from "../../../../SharedComponents/CommonMatTable";
+import InstallmentPaymentDialog from "./InstallmentPaymentDialog";
+import { set } from "react-hook-form";
 
 const AdmissionFeesView = () => {
     const [admissionFeesList, setAdmissionFeesList] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
     const userData = JSON.parse(localStorage.getItem("UserData") || "{}");
+    const [paymentModes, setPaymentModes] = useState([]);
+    const [open, setOpen] = useState(false);
+    const [selectedFeesData, setSelectedFeesData] = useState({});
 
     const fetchAdmissionFeesList = useCallback(async () => {
         setIsLoading(true);
         const payload = { "user_ids": [] };
         if (userData?.role === 'PARENT') {
-            if(userData?.student_info?.length > 0) {
+            if (userData?.student_info?.length > 0) {
                 payload.user_ids = userData.student_info.map(student => student.student_id);
             }
         }
         try {
             const response = await fetchAdmissionFeesInfo(payload);
-            setAdmissionFeesList(response?.data?.admission_fees_data || []);
+            setAdmissionFeesList(response.data.admission_fees_data || []);
         } catch (err) {
             console.error("Failed to fetch admission fees list:", err);
         } finally {
@@ -28,7 +33,25 @@ const AdmissionFeesView = () => {
 
     useEffect(() => {
         fetchAdmissionFeesList();
+        fetchPaymentModesList();
     }, []);
+
+    const fetchPaymentModesList = async () => {
+        try {
+            const response = await fetchPaymentModes();
+            setPaymentModes(response.data.payment_modes || []);
+        } catch (err) {
+            console.error("Failed to fetch payment modes:", err);
+        }
+    };
+
+    const handleClose = (isSubmit) => {
+        setOpen(false);
+        setSelectedFeesData({});
+        if (isSubmit) {
+            fetchAdmissionFeesList();
+        }
+    };
 
     const columns = useMemo(
         () => [
@@ -38,32 +61,55 @@ const AdmissionFeesView = () => {
             { accessorKey: "total_admission_charge", header: "Total Charge" },
             { accessorKey: "deposited_fees", header: "Deposited Fees" },
             { accessorKey: "total_installment_due", header: "Total Due" },
-            { accessorKey: "total_emi_amount", header: "Total EMI Amount" },
             { accessorKey: "number_of_installments", header: "Installments" },
             { accessorKey: "installment_amount", header: "Installment Amount" },
-            { accessorKey: "first_installment_due_date", header: "First Installment Due" },
-            { accessorKey: "first_installment_paid_status", header: "First Installment Paid" },
-            { accessorKey: "second_installment_due_date", header: "Second Installment Due" },
-            { accessorKey: "second_installment_paid_status", header: "Second Installment Paid" },
-            { accessorKey: "third_installment_due_date", header: "Third Installment Due" },
-            { accessorKey: "third_installment_paid_status", header: "Third Installment Paid" },
-            { accessorKey: "fourth_installment_due_date", header: "Fourth Installment Due" },
-            { accessorKey: "fourth_installment_paid_status", header: "Fourth Installment Paid" },
-            { accessorKey: "fifth_installment_due_date", header: "Fifth Installment Due" },
-            { accessorKey: "fifth_installment_paid_status", header: "Fifth Installment Paid" },
-            { accessorKey: "sixth_installment_due_date", header: "Sixth Installment Due" },
-            { accessorKey: "sixth_installment_paid_status", header: "Sixth Installment Paid" },
+            {
+                header: "Payment",
+                accessorFn: (row) => {
+                    const isAllInstallmentsPaid = [];
+            
+                    if (Array.isArray(row.installments) && row.installments.length > 0) {
+                        row.installments.forEach(element => {
+                            if (!element.paid_status) {
+                                isAllInstallmentsPaid.push(element);
+                            }
+                        });
+                    }
+            
+                    console.log("isAllInstallmentsPaid", isAllInstallmentsPaid);
+            
+                    return row.is_deposit_paid && isAllInstallmentsPaid.length === 0 ? (
+                        "Paid"
+                    ) : (
+                        <div className="d-flex align-items-center justify-content-between flex-wrap">
+                            <Button
+                                size="small"
+                                variant="contained"
+                                onClick={() => {
+                                    setSelectedFeesData(row);
+                                    setOpen(true);
+                                }}
+                            >
+                                Make Payment
+                            </Button>
+                        </div>
+                    );
+                },
+            }            
         ],
         []
     );
 
     return (
-        <CommonMatTable
-            columns={columns}
-            isLoading={isLoading}
-            data={admissionFeesList}
-            renderTopToolbar={() => <Typography variant="h6">Admission Fees</Typography>}
-        />
+        <>
+            <CommonMatTable
+                columns={columns}
+                isLoading={isLoading}
+                data={admissionFeesList}
+                renderTopToolbar={() => <Typography variant="h6">Admission Fees</Typography>}
+            />
+            <InstallmentPaymentDialog open={open} onClose={handleClose} feesData={selectedFeesData} paymentModes={paymentModes} />
+        </>
     );
 };
 
